25700615d4e81dd30cd528903c6ef0b34d525aca
rbulliard <45511889+rbulliard@users.noreply.github.com>
2018-12-01 22:20:18 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'where-is-alex';
+  
+  onFileChanged(event) {
+    const file = event.target.files[0];
+    this.http.post('my-backend.com/file-upload', this.selectedFile).subscribe(event => {
+      console.log(event); // handle event here
+    });
+  }
+  
 }
