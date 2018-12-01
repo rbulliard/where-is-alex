4a4d186156173939365e86d8826e6ccc536104f0
rbulliard <45511889+rbulliard@users.noreply.github.com>
2018-12-01 22:59:36 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,23 @@ export class AppComponent {
   
   constructor(private http: HttpClient) {}
   
-  const urlModel = "https://automl.googleapis.com/v1beta1/projects/endless-upgrade-223916/locations/us-central1/models/ICN5541762132522833103";
+  var urlModel = "https://automl.googleapis.com/v1beta1/projects/endless-upgrade-223916/locations/us-central1/models/ICN5541762132522833103";
   
   onFileChanged(event) {
-    var postObject = new Object();
-    postObject.payload.image.imageBytes = event.target.files[0];
-    this.http.post(urlModel, postObject).subscribe(event => {
-      console.log(event); // handle event here
-    });
+    this.http.post(
+      urlModel, 
+      {
+        payload : {
+         image : {
+           imageBytes : event.target.files[0]
+          }
+        }
+      }, 
+      {
+       headers: { 'Content-Type': 'application/json' }
+      }).subscribe(event => {
+        console.log(event); // handle event here
+      });
   }
   
 }
