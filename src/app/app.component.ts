e151a14f3d4c804ddfd9e63db58a8acb1ba20a42
rbulliard <45511889+rbulliard@users.noreply.github.com>
2018-12-02 13:45:51 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,9 +19,10 @@ export class AppComponent {
     };
     //var urlModel = "https://automl.googleapis.com/v1beta1/projects/endless-upgrade-223916/locations/us-central1/models/ICN5541762132522833103:predict";
     var url = "https://us-central1-endless-upgrade-223916.cloudfunctions.net/where-is-alex";
-    this.http.post(url, event.target.files[0], config).subscribe(event => {
-        console.log(event); // handle event here
-      });
+    this.http.post(url, event.target.files[0], config);
+      //.subscribe(event => {
+      //  console.log(event); // handle event here
+      //});
   }
   
 }
