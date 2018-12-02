26df0a73fbb20d20a6b61f9be5f2e1d05f0a4353
rbulliard <45511889+rbulliard@users.noreply.github.com>
2018-12-02 13:09:48 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent {
   constructor(private http: HttpClient) {}
   
   onFileChanged(event) {
+    console.log(event.target.files[0]);
     //var urlModel = "https://automl.googleapis.com/v1beta1/projects/endless-upgrade-223916/locations/us-central1/models/ICN5541762132522833103:predict";
     var url = "https://us-central1-endless-upgrade-223916.cloudfunctions.net/where-is-alex";
     this.http.post(url, event.target.files[0]).subscribe(event => {
